514daddc904251631c61b221b6892cb46ab0be70
Julio2408 <julioanthonio8@gmail.com>
2025-03-18 17:49:49 -0500

    actualiza

--- app.js
+++ app.js
@@ -1,53 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = [];
+let listaAmigos = [];
+const inputAmigo = document.getElementById("amigo");
+const ulListaAmigos = document.getElementById("listaAmigos");
+const ulResultado = document.getElementById("resultado");
 
 function agregarAmigo(){
-    const inputAmigo = document.getElementById(`amigo`);
-    const nombreAmigo = inputAmigo.Value.trim();
 
-    if(nombre===""){
-        alert("Por favor inserte un nombre");
-        return;
-    }
-
-    if(amigos.includes(nombreAmigo)){
-        alert(`El nombre ${nombreAmigo} ya esta en la lista` );
-        return;
-    }
-
-    amigos.push(nombreAmigo);
-
-    inputAmigo.Value = "";
-
-    actualizarLista();
+    listaAmigos.push(inputAmigo.value);
+   ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+    
 }
 
-function actualizarLista(){
 
-    const listaAmigos = document.getElementById(`listaAmigos`);
-
-    listaAmigos.innerHTML="";
-
-    for (let i=0;i<amigos.length;i++){
-        const li= document.createElement('li');
-        li.textContent=amigos[i];
-        listaAmigos.appendChild(li);
-    }
-}
 
 function sortearAmigo(){
 
-    if (amigos.length===0){
+const random =Math.floor((Math.random() * listaAmigos.length))
+const amigoSecreto= listaAmigos[random];
 
-        alert("No hay amigos disponibles para sortear");
-        return;
-    }
+ulResultado.innerHTML=`<li>${amigoSecreto}</li>`; 
 
-    const indiceAleatorio=Math.floor(Math.random() * amigos.length);
-
-    const amigoSorteado = amigos(indiceAleatorio);
-    
-    const resultado= document.getElementById('resultado');
-    
-    resultado.innerHTML= `Amigos sorteado: <strong>${amigoSorteado}</strong>`;
 }
